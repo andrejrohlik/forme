041bdb456d093e58a7ba73e6874ccd41c960e697
andrejrohlik <andrej.rohlik@skole.hr>
2016-03-10 13:54:53 +0100

    Ovo je drugi commit. Tu smo izmijenili boje za spol. Muško je plava boja, a žensko je crvena boja.

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="index.css" />
     <title>Naslov</title>
 </head>
 <body>
@@ -9,17 +10,41 @@
 
 <p>
 
-<form>
+<form method="post">
     Ime:<br>
     <input type="text" name="ime"><br>
     Prezime:<br>
     <input type="text" name="prezime"><br>
-    <input type="submit" value="Idu dani!">
+    Spol:<br>
+    <p class="musko">
+    <input type="radio" name="spol" value="M">Muško<br>
+    </p>
+    <p class="zensko">
+    <input type="radio" name="spol" value="Ž">Žensko<br>
+    </p>
+    <input type="submit" value="Pošalji!">
 </form>
 
 <?php
-echo('Vaše ime je: ' . htmlspecialchars($_GET['ime']) . '<br>');
-echo('Vaše prezime je: ' .$_GET['prezime']);
+
+echo('Vaše ime je: ' . htmlspecialchars($_POST['ime']) . '<br>');
+echo('Vaše prezime je: ' . htmlspecialchars($_POST['prezime']) . '<br>');
+echo('Vaš spol je: ' . htmlspecialchars($_POST['spol']). '<br>');
+
+if ($_POST['spol'] == 'M')
+{
+    echo('<p class="musko">' . 'Vi ste pravi muškarac!' . '</p>');
+}
+
+elseif ($_POST['spol'] == 'Ž')
+{
+    echo('<p class="zensko">' . 'Vi ste prava žena!' . '</p>');
+}
+else
+{
+    echo('A što ste onda!?');
+}
+
 ?>
 
 </p>
